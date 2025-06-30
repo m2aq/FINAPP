cd879370f3a7ab2f0ed8ac973215ada3c7c1c074
m2aq <feizvuk@gmail.com>
2025-06-30 10:18:33 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", function(e) {
-  console.log("Service Worker instalado");
-});
-
-self.addEventListener("fetch", function(e) {
-  // Puedes interceptar solicitudes aquí si quieres
-});
